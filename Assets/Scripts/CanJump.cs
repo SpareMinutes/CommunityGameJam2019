1172dea20f05c09ed49c1a2ed56d59080eaf7f77
NULL000 <49639934+NULL1FY3R@users.noreply.github.com>
2019-08-26 15:05:41 +1000

    implemented a basic jump that has increased falling speed

---
+++ Assets/Scripts/CanJump.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanJump : MonoBehaviour
+{
+    public GameObject Player;
+
+    void OnTriggerEnter2D () {
+        Player.GetComponent<Movement>().canJump = true;
+    }
+
+    void OnTriggerExit2D () {
+        Player.GetComponent<Movement>().canJump = false;
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed;
+    public float speed, jumpForce, goingDownGravity, goingUpGravity;
+    public bool canJump = true;
     private float new_x;
+    private Rigidbody2D rb;
+    
+
+    void Start () {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
 
     void Update() {
         //get inputs
         new_x = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump) {
+            Jump();
+        }
+
+        if (rb.velocity.y < 0) {
+            //increase gravity for the faster falling
+            rb.gravityScale = goingDownGravity;
+        } else {
+            rb. gravityScale = goingUpGravity;
+        }
     }
 
     void FixedUpdate () {
         //move the gameobject
         gameObject.transform.position += new Vector3 (new_x*speed, gameObject.transform.position.y, gameObject.transform.position.z) * Time.fixedDeltaTime;
     }
+
+    void Jump () {
+        rb.AddForce(new Vector3(0,jumpForce,0));
+    }
+
+    
 }
