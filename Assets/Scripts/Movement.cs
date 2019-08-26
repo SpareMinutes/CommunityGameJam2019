d0afa566b96938dd4a08478f8033984b97f10132
NULL000 <49639934+NULL1FY3R@users.noreply.github.com>
2019-08-26 13:27:17 +1000

    left/right movement done

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    private float new_x;
+
+    void Update() {
+        //get inputs
+        new_x = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate () {
+        //move the gameobject
+        gameObject.transform.position += new Vector3 (new_x*speed, gameObject.transform.position.y, gameObject.transform.position.z) * Time.fixedDeltaTime;
+    }
+}
