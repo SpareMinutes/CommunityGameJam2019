d1f21840542ff009ef9b530ca9781be921dfb095
MegaMinerd <megaminerd@gmail.com>
2019-08-28 17:46:52 -0400

    Added menu fuctionality

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour{
+    [SerializeField]
+    private GameObject[] Screens;
+
+    public void Start() {
+        RestoreSettings();
+        SetActiveScreen(0);
+    }
+
+    public void SetActiveScreen(int id){
+        for(int i=0; i<8; i++){
+            Screens[i].SetActive(i == id);
+        }
+    }
+
+    public void RestoreSettings() {
+        GameObject.Find("VSync").GetComponent<Toggle>().isOn = Settings.GetBool("VSync");
+        GameObject.Find("VBO").GetComponent<Toggle>().isOn = Settings.GetBool("VBO");
+        GameObject.Find("Anaglyph").GetComponent<Toggle>().isOn = Settings.GetBool("Anaglyph");
+        GameObject.Find("RTX").GetComponent<Toggle>().isOn = Settings.GetBool("RTX");
+        GameObject.Find("Particles").GetComponent<Toggle>().isOn = Settings.GetBool("Particles");
+        GameObject.Find("Physics").GetComponent<Toggle>().isOn = Settings.GetBool("Physics");
+        GameObject.Find("PPhysics").GetComponent<Toggle>().isOn = Settings.GetBool("PPhysics");
+        GameObject.Find("GAccel").GetComponent<Toggle>().isOn = Settings.GetBool("GAccel");
+        GameObject.Find("PAccel").GetComponent<Toggle>().isOn = Settings.GetBool("PAccel");
+        GameObject.Find("Collisions").GetComponent<Toggle>().isOn = Settings.GetBool("Collisions");
+        GameObject.Find("PCollisions").GetComponent<Toggle>().isOn = Settings.GetBool("PCollisions");
+        GameObject.Find("Narrator").GetComponent<Toggle>().isOn = Settings.GetBool("Narrator");
+        GameObject.Find("Variance").GetComponent<Toggle>().isOn = Settings.GetBool("Variance");
+        GameObject.Find("Harmonics").GetComponent<Toggle>().isOn = Settings.GetBool("Harmonics");
+
+        GameObject.Find("Mipmaps").GetComponent<Slider>().value = Settings.GetFloat("Mipmaps");
+        GameObject.Find("Sounds").GetComponent<Slider>().value = Settings.GetFloat("Sounds");
+        GameObject.Find("Music").GetComponent<Slider>().value = Settings.GetFloat("Music");
+    }
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour{
+    private static Settings Instance;
+    private Dictionary<string, bool> Bools;
+    private Dictionary<string, float> Floats;
+
+    public void Start(){
+            Instance = new Settings(
+                new Dictionary<string, bool>{
+                    {"VSync", false},
+                    {"VBO", false},
+                    {"Anaglyph", false},
+                    {"RTX", false},
+                    {"Particles", true},
+                    {"Physics", true},
+                    {"PPhysics", true},
+                    {"GAccel", true},
+                    {"PAccel", true},
+                    {"Collisions", true},
+                    {"PCollisions", true},
+                    {"Narrator", false},
+                    {"Variance", false},
+                    {"Harmonics", false}
+                },
+                new Dictionary<string, float>{
+                    {"Mipmaps", 1},
+                    {"Sounds", 1},
+                    {"Music", 1}
+                }
+            );
+    }
+
+    private Settings(Dictionary<string, bool> b, Dictionary<string, float> f){
+        this.Bools = b;
+        this.Floats = f;
+    }
+
+    public static bool GetBool(string name){
+        return Instance.Bools[name];
+    }
+
+    public static float GetFloat(string name){
+        return Instance.Floats[name];
+    }
+
+    public void ToggleBool(string name){
+        Instance.Bools[name] = !Instance.Bools[name];
+    }
+
+    public void UpdateFloat(string name){
+        Instance.Floats[name] = GameObject.Find(name).GetComponent<Slider>().value;
+    }
+}
