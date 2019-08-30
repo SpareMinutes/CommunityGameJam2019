c88048a2685007ca95acaff4ed45defb605302da
MegaMinerd <megaminerd@gmail.com>
2019-08-30 12:04:41 -0400

    Added goals

--- Assets/Scripts/RespawnAndGoal.cs
+++ Assets/Scripts/RespawnAndGoal.cs
@@ -6,10 +6,13 @@ using UnityEngine.SceneManagement;
 public class RespawnAndGoal : MonoBehaviour {
     [SerializeField]
     private BoxCollider2D respawner, goal;
+    [SerializeField]
+    private string nextScene;
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        Debug.Log("run");
         if(GetComponentInParent<CircleCollider2D>().IsTouching(respawner))
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (GetComponentInParent<CircleCollider2D>().IsTouching(goal))
+            SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
     }
 }
