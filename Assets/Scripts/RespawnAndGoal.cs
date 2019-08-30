948be93029068a66456293f3d2a5f06a11ebe7a3
MegaMinerd <megaminerd@gmail.com>
2019-08-30 11:32:38 -0400

    Added respawning

---
+++ Assets/Scripts/RespawnAndGoal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RespawnAndGoal : MonoBehaviour {
+    [SerializeField]
+    private BoxCollider2D respawner, goal;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("run");
+        if(GetComponentInParent<CircleCollider2D>().IsTouching(respawner))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
