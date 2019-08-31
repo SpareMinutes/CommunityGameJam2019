9cda6695da0a6e94a97d5d538ab6e7e6a0ea2a7e
NULL000 <49639934+NULL1FY3R@users.noreply.github.com>
2019-08-31 20:59:05 +1000

    added a basic music player
    need to put the music in the audio source in the game object

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+    AudioSource audioData;
+
+    void Start() {
+        audioData = GetComponent<AudioSource>();
+        audioData.Play();
+        Debug.Log("started playing music");
+    }
+}
