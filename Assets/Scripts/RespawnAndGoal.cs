d90e236aef256fee7f4c37770ce325f3df1e48f7
MegaMinerd <megaminerd@gmail.com>
2019-08-30 22:42:58 -0400

    Added crushing by moving platform

--- Assets/Scripts/RespawnAndGoal.cs
+++ Assets/Scripts/RespawnAndGoal.cs
@@ -15,8 +15,8 @@ public class RespawnAndGoal : MonoBehaviour {
         if (GetComponentInParent<CircleCollider2D>().IsTouching(teaser))
             return;
         //There are no other types of colliders. If this is reached, we can assume it's a respawner.
-        //Only check if the player is on the ground.
-        if(GetComponentInParent<Movement>().CanJump())
+        //Only check if the player jumped into it or if they fell/got squished
+        if(GetComponentInParent<Rigidbody2D>().velocity.y<=0.0001)
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
